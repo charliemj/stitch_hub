1ab74ad1884f9de25594f631a66dc9f32a70232c
Denis Li <denisli@mit.edu>
2016-11-19 16:35:36 -0500

    specified interface for ChartModel

---
+++ stitch_hub/public/javascript/chart_model.js
@@ -0,0 +1,34 @@
+var ChartModel = function() {
+  var that = Object.create(ChartModel.prototype);
+
+  /**
+  * Returns row size.
+  */
+  that.getRowSize = function() {
+
+  };
+
+  /**
+  * Returns column size.
+  */
+  that.getColSize = function() {
+
+  };
+
+  /**
+  * Get hex value (representing color) of a given cell.
+  */
+  that.getColor = function(row, col) {
+
+  };
+
+  /**
+  * Sets hex value (representing color) of a given cell
+  */
+  that.setColor = function(row, col, color) {
+
+  };
+
+  Object.freeze(that);
+  return that;
+};
