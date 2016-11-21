52619635e0df033967f2fe6993c3609aaa4a2389
Karleigh Moore <karleighmoore19@gmail.com>
2016-11-21 17:34:41 -0500

    added validation to model

--- stitch_hub/model/chart_model.js
+++ stitch_hub/model/chart_model.js
@@ -1,15 +1,30 @@
 var mongoose = require('mongoose');
+//things we can easily validate with this package: https://www.npmjs.com/package/mongoose-validators
+//can validate one or multiple things
+// to validate multiple fields, put in a list
+
+// single validator like this:
+// var Schema = new mongoose.Schema({
+//     email: {type: String, validate: validators.isEmail()}
+// });
+
+// multiple validators like this:
+// var Schema = new mongoose.Schema({
+//     username: {type: String, validate: [validators.isAlphanumeric(), validators.isLength(2, 60)]}
+// });
+
+var validators = require('mongoose-validators');
+
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
-
 var chartSchema = mongoose.Schema({
-    title: String,
-    description: String,
-    date: { type: Date, default: Date.now },
-    type: String, //cross stitch, knitting,crochet 
-    rowSize: Number,
-    colSize: Number,
-    rows:[[String]], //if things break look at this
+    title: {type:String},
+    description: {type:String},
+    date: { type: Date, default: Date.now,validate: validators.isDate() },
+    type: {type:String, validate: validators.matches('KNIT_V', 'CROSS_STICH','CROCHET_V','KNIT_H','CROCHET_H')},
+    rowSize: {type:Number},
+    colSize: {type:Number},
+    rows:[[{type:String, validate: validators.isHexColor()}]], //if things break look at this
     parent: {type:ObjectId,ref:"Chart"}
 });
 
