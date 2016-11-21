572c16bf92e34e5767ee9238f0b3b1ef2722ed75
James Usrey <jusrey@mit.edu>
2016-11-20 19:03:01 -0500

    Rewrote chart_editing.html to work with new javascript
    added gridlines to canvas
    prepared for mustache integration

--- stitch_hub/public/javascript/chart_controller.js
+++ stitch_hub/public/javascript/chart_controller.js
@@ -1,13 +1,33 @@
-var ChartController = function(view, model, colorPicker) {
-  var that = Object.create(ChartController.prototype);
-
-  canvas.addEventListener('mouseclick', function(evt) {
-    var cell = view.getCell(evt);
-    var color = colorPicker.value;
+function ClickChange(view, model, color){
+	var cell = view.getCell(event);
     view.colorCell(cell.row, cell.col, color);
     model.setColor(cell.row, cell.col, color);
-  });
+}
 
-  Object.freeze(that);
-  return that;
-};
+function ChangeType(scale, model, canvas){
+	var temp = scale.split(',');
+	var xscale = temp[0];
+	var yscale = temp[1];
+
+	return ChartView(xscale, yscale, model, canvas)
+}
+
+
+function Save(){
+
+	console.log(document.getElementById('typeSelect').value.split(',')[2]);
+
+
+
+
+	var t = document.getElementById('title').value;
+	var d = document.getElementById('description').value;
+	var rs = document.getElementById('rowSize').value;
+	var cs = document.getElementById('colSize').value;
+	var ct = document.getElementById('type').value;
+	var r = document.getElementById('rows').value;
+
+	console.log("Title", t,d);
+	console.log("ChartType", ct);
+	console.log("Rows", r);
+}
--- stitch_hub/public/javascript/chart_view.js
+++ stitch_hub/public/javascript/chart_view.js
@@ -9,19 +9,37 @@ var ChartView = function(cellWidth, cellHeight, model, canvas) {
   // Initialize by clearing the canvas first
   var canM = canvas.getContext("2d");
   canM.clearRect(0, 0, canvas.width, canvas.height);
+  for (var i=0; i < colSize; i++) {
+      for (var j=0; j < rowSize; j++) {
+        canM.fillStyle = '#000000';
+        canM.fillRect(cellWidth*i,cellHeight*j, cellWidth, cellHeight);
+        canM.fillStyle = '#FFFFFF';
+        canM.fillRect((cellWidth)*i + 1 ,(cellHeight)*j + 1, (cellWidth-1), (cellHeight-1));
+      }
+    }
+
+
 
   that.draw = function() {
     for (var i=0; i < colSize; i++) {
       for (var j=0; j < rowSize; j++) {
         colorToUse = model.getColor(j, i);
-        canM.fillStyle = colorToUse;
+        // canM.fillStyle = colorToUse;
+        // canM.fillRect(cellWidth*i,cellHeight*j, cellWidth, cellHeight);
+
+        canM.fillStyle = '#000000';
         canM.fillRect(cellWidth*i,cellHeight*j, cellWidth, cellHeight);
+        canM.fillStyle = colorToUse;
+        canM.fillRect((cellWidth)*i + 1 ,(cellHeight)*j + 1, (cellWidth-1), (cellHeight-1));
       }
+    }
   };
 
   that.colorCell = function(row, col, color) {
-    canM.fillStyle = color;
+    canM.fillStyle = '#000000';
     canM.fillRect(cellWidth*col,cellHeight*row, cellWidth, cellHeight);
+    canM.fillStyle = color;
+    canM.fillRect((cellWidth)*col + 1 ,(cellHeight)*row + 1, (cellWidth-1), (cellHeight-1));
   };
 
   that.getCanvas = function() {
@@ -47,10 +65,18 @@ var CrossStitchChartView = function(model, canvas) {
   return ChartView(10, 10, model, canvas);
 };
 
-var KnitChartView = function() {
+var KnitChartViewVert = function(model, canvas) {
   return ChartView(10, 20, model, canvas);
 };
 
-var CrochetChartView = function() {
+var CrochetChartViewVert = function(model, canvas) {
   return ChartView(10, 15, model, canvas);
+};
+
+var KnitChartViewHoriz = function(model, canvas) {
+  return ChartView(20, 10, model, canvas);
+};
+
+var CrochetChartViewHoriz = function(model, canvas) {
+  return ChartView(15, 10, model, canvas);
 };
