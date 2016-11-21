573ae86b221b507ea913c7396c2b395406d911d8
Karleigh Moore <karleighmoore19@gmail.com>
2016-11-21 12:29:04 -0500

    all routes should now work (need to uncomment the routing logic though)

--- stitch_hub/routes/charts_routes.js
+++ stitch_hub/routes/charts_routes.js
@@ -5,21 +5,22 @@ var Charts = require('../model/chart_model.js');
 
 //GET charts/:id
 router.get('/:id',function(req, res, next){
-    var chartId = req.body.id;
-    Charts.find({_id:chartId})
-    .exec(function(err,chart){
-         if (err) {
-             res.send({
-                 success: false,
-                 message: err
-             }); //end if
-         } else{
-            res.send({
-                sucess:true,
-                message:chart
-            })
-        }//end else
-    })
+    res.sendStatus(200);
+    // var chartId = req.body.id;
+    // Charts.find({_id:chartId})
+    // .exec(function(err,chart){
+    //      if (err) {
+    //          res.send({
+    //              success: false,
+    //              message: err
+    //          }); //end if
+    //      } else{
+    //         res.send({
+    //             sucess:true,
+    //             message:chart
+    //         })
+    //     }//end else
+    // })
 });
 
 // Look at the link below to view solution to Cannot GET issue
@@ -63,28 +64,29 @@ router.get('/',function(req, res, next){
 
 //POST charts/
 router.post('/', function(req,res, next){
-    var title = req.body.title; //make sure view is named correctly
-    var description = req.body.description;
-    // var date = 
-    var type = req.body.type;
-    var rowSize = req.body.rowSize;
-    var colSize = req.body.colSize;
-    var rows = req.body.rows;
-    var parent = req.body.parent;
+    res.sendStatus(200);
+    // var title = req.body.title; //make sure view is named correctly
+    // var description = req.body.description;
+    // // var date = 
+    // var type = req.body.type;
+    // var rowSize = req.body.rowSize;
+    // var colSize = req.body.colSize;
+    // var rows = req.body.rows;
+    // var parent = req.body.parent;
 
-    Charts.create({"title":title,"description":description,
-        "type":type,"rowSize":rowSize,"colSize":colSize,"rows":rows,"parent":parent}, 
-        function(err,chart){
-            if (err) {
-                res.send({
-                    success: false,
-                    message: err
-                }); //end if
-            } else{
-                res.redirect("/"); //eventually want to redirect to newly created chart page
-            }
-        }
-    );
+    // Charts.create({"title":title,"description":description,
+    //     "type":type,"rowSize":rowSize,"colSize":colSize,"rows":rows,"parent":parent}, 
+    //     function(err,chart){
+    //         if (err) {
+    //             res.send({
+    //                 success: false,
+    //                 message: err
+    //             }); //end if
+    //         } else{
+    //             res.redirect("/"); //eventually want to redirect to newly created chart page
+    //         }
+    //     }
+    // );
 });
 
 
