474b8ca9485de478ba8cf9575956507945d900b2
Denis Li <denisli@mit.edu>
2016-11-22 18:13:24 -0500

    using mustache.js instead of javascript for rendering news feed

--- stitch_hub/public/javascript/index.js
+++ stitch_hub/public/javascript/index.js
@@ -3,8 +3,31 @@ $(document).ready(function() {
     url: '/charts',
     method: 'GET',
     success: function(charts) {
-      charts.forEach(function(chartJson) {
-        addChart(chartJson);
+      // fill in the template with the charts
+      var template = $('#charts-template').html();
+      var html = Mustache.render(template, { charts: charts });
+      $('.charts-container').append(html);
+
+      // draw each chart
+      $('.chart-canvas').each(function(i, canvas) {
+        var jcanvas = $(canvas);
+        var id = jcanvas.attr('data-id');
+        var chartJson = findChartWithId(charts, id);
+        console.log(chartJson);
+        var chartModel = getChartFromJson(chartJson);
+        var chartView = CrossStitchChartView(chartModel, canvas);
+        chartView.draw();
+      });
+
+      // add remix button for each chart
+      $('.remix-button').each(function(i, button) {
+        var jbutton = $(button);
+        var id = jbutton.attr('data-id');
+        var chartJson = findChartWithId(charts, id);
+        jbutton.on('click', function() {
+          window.sessionStorage.setItem('chart', JSON.stringify(chartJson));
+          window.location = "chart_editing.html";
+        });
       });
     },
     error: function(error) {
@@ -14,6 +37,15 @@ $(document).ready(function() {
   });
 });
 
+var findChartWithId = function(charts, id) {
+  for (var i = 0; i < charts.length; i++) {
+    var chartJson = charts[i];
+    if (chartJson._id == String(id)) {
+      return chartJson;
+    }
+  }
+  throw "Should not get here"; // should not get here
+}
 
 
 var makeChart = function() {
