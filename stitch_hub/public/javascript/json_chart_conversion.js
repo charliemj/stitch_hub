80f7faa1bd1c17220f97e65d53ffb299d9661a24
James Usrey <jusrey@mit.edu>
2016-11-22 03:24:12 -0500

    fixed error in chartJson to chart model logic.

--- stitch_hub/public/javascript/json_chart_conversion.js
+++ stitch_hub/public/javascript/json_chart_conversion.js
@@ -6,7 +6,7 @@ var getChartFromJson = function(chartJson) {
   for (var i = 0; i < rowSize; i++) {
     for (var j = 0; j < colSize; j++) {
 
-    	var index = i*rowSize + j;
+    	var index = i*colSize + j;
     	var colors = chartJson.rows[0].split(",");
 
       chart.setColor(i, j, colors[index]);
