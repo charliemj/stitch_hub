c26a67035b30a536bdb5b992ff523cd384b21fcf
Denis Li <denisli@mit.edu>
2016-11-22 17:15:21 -0500

    refactored inlined scripting for posting charts and canvas drawing out to chart_editing.js

--- stitch_hub/public/javascript/chart_editing.js
+++ stitch_hub/public/javascript/chart_editing.js
@@ -1,21 +1,53 @@
-var jsonChart = JSON.parse(window.sessionStorage.getItem('chart'));
+$(document).ready(function() {
+  // fetch the chart JSON
+  var jsonChart = JSON.parse(window.sessionStorage.getItem('chart'));
 
-scaledata = getRatio(jsonChart.type);
-var xscale = scaledata[1];
-var yscale = scaledata[2];
-var TypeOfChart = jsonChart.type;
-document.getElementById("typeSelect").value = scaledata[1] +','+ scaledata[2] +','+ jsonChart.type;
+  // store information about the type of chart into the type selector
+  scaledata = getRatio(jsonChart.type);
+  var xscale = scaledata[1];
+  var yscale = scaledata[2];
+  var TypeOfChart = jsonChart.type;
+  document.getElementById("typeSelect").value = scaledata[1] +','+ scaledata[2] +','+ jsonChart.type;
 
+  // color the canvas based on the given chart
+  var canvas = document.getElementById("canvas");
+  var chartType = jsonChart.type;
+  var model = getChartFromJson(jsonChart);
+  var view = ChartView(xscale,yscale,model,canvas);
+  view.draw();
 
-var XDIM = jsonChart.rowSize;
-var YDIM = jsonChart.colSize;
+  // add event listener for coloring the canvas based on clicks
+  $(canvas).on('click', function() {
+    ClickChange(view,model,document.getElementById("html5colorpicker").value);
+  });
 
+  // add event listener so that post-chart-button will post when clicked
+  $('#post-chart-button').on('click', function() {
+    var stringifiedRows = JSON.stringify(model.getRows());
+    $.ajax({
+      url: '/charts',
+      method: 'POST',
+      data: {
+        title: document.getElementById("title").value,
+        description: document.getElementById("description").value,
+        rowSize: model.getRowSize(),
+        colSize: model.getColSize(),
+        type: TypeOfChart,
+        rows: stringifiedRows,
+        parent: jsonChart.id
+      },
+      success: function() {
+        console.log("successfully posted chart");
+        window.location.replace("/");
+      },
+      error: function(error) {
+        console.log('Error posting charts');
+        console.log(error);
+      },
+    });
+  });
 
-var canvas = document.getElementById("canvas");
-var chartType = jsonChart.type;
-var model = getChartFromJson(jsonChart);
-var view = ChartView(xscale,yscale,model,canvas);
-view.draw();
+});
 
 
 var changeType = function() {
@@ -24,34 +56,3 @@ var changeType = function() {
   TypeOfChart = temporary;
   view.draw();
 }
-
-var PostChart = function(){
-
-$(document).ready(function() {
-
-  var stringifiedRows = JSON.stringify(model.getRows());
-  $.ajax({
-    url: '/charts',
-    method: 'POST',
-    data: {
-      title: document.getElementById("title").value,
-      description: document.getElementById("description").value,
-      rowSize: XDIM,
-      colSize: YDIM,
-      type: TypeOfChart,
-      rows: stringifiedRows,
-      parent: jsonChart.id
-    },
-    success: function() {
-      console.log("successfully posted chart");
-      window.location.replace("/");
-    },
-    error: function(error) {
-      console.log('Error posting charts');
-      console.log(error);
-    },
-
-
-  });
-});
-};
