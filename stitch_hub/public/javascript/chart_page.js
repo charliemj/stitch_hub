69d541f249568c4246107cf2bb1e51973e65d86b
Val Healy <valiant@mit.edu>
2016-11-23 00:10:46 -0500

    added chart page title and description

--- stitch_hub/public/javascript/chart_page.js
+++ stitch_hub/public/javascript/chart_page.js
@@ -7,9 +7,11 @@ $(document).ready(function() {
   scaleData = getRatio(jsonChart.type);
   var xscale = scaleData[1]*2;
   var yscale = scaleData[2]*2;
-  console.log("param for ChartView: " + xscale + " " + yscale + " " + model + " " + canvas);
   var view = ChartView(xscale,yscale,model,canvas);
   view.draw();
+
+  $('<h1>'+jsonChart.title+'</h1>').appendTo('#chart-info');
+  $('<p>'+jsonChart.description+'</p>').appendTo('#chart-info');
 
   $('.remix-button').each(function(i, button) {
     var jbutton = $(button);
