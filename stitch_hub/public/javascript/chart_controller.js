c8356003f9b02a7dc3812bb013e3a774e70ebc7b
Denis Li <denisli@mit.edu>
2016-11-22 19:31:09 -0500

    made the gridding visual of the charts better

--- stitch_hub/public/javascript/chart_controller.js
+++ stitch_hub/public/javascript/chart_controller.js
@@ -1,7 +1,9 @@
 var ClickChange = function (view, model, color){
 	var cell = view.getCell(event);
+  if (cell != null) {
     view.colorCell(cell.row, cell.col, color);
     model.setColor(cell.row, cell.col, color);
+  }
 }
 
 var ChangeSize = function (xscale, yscale, model, canvas){
--- stitch_hub/public/javascript/chart_view.js
+++ stitch_hub/public/javascript/chart_view.js
@@ -1,45 +1,29 @@
 var ChartView = function(cellWidth, cellHeight, model, canvas) {
   var that = Object.create(ChartView.prototype);
 
+  var gapSize = 1;
   var rowSize = model.getRowSize();
   var colSize = model.getColSize();
-  canvas.width = cellWidth * colSize;
-  canvas.height = cellHeight * rowSize;
+  canvas.width = cellWidth * colSize + (colSize + 1) * gapSize;
+  canvas.height = cellHeight * rowSize + (rowSize + 1) * gapSize;
 
   // Initialize by clearing the canvas first
   var canM = canvas.getContext("2d");
-  canM.clearRect(0, 0, canvas.width, canvas.height);
-  for (var i=0; i < colSize; i++) {
-      for (var j=0; j < rowSize; j++) {
-        canM.fillStyle = '#000000';
-        canM.fillRect(cellWidth*i,cellHeight*j, cellWidth, cellHeight);
-        canM.fillStyle = '#FFFFFF';
-        canM.fillRect((cellWidth)*i + 1 ,(cellHeight)*j + 1, (cellWidth-1), (cellHeight-1));
-      }
-    }
-
-
 
   that.draw = function() {
     for (var i=0; i < colSize; i++) {
       for (var j=0; j < rowSize; j++) {
         colorToUse = model.getColor(j, i);
-        // canM.fillStyle = colorToUse;
-        // canM.fillRect(cellWidth*i,cellHeight*j, cellWidth, cellHeight);
-
-        canM.fillStyle = '#000000';
-        canM.fillRect(cellWidth*i,cellHeight*j, cellWidth, cellHeight);
-        canM.fillStyle = colorToUse;
-        canM.fillRect((cellWidth)*i + 1 ,(cellHeight)*j + 1, (cellWidth-1), (cellHeight-1));
+        that.colorCell(j, i, colorToUse);
       }
     }
   };
 
   that.colorCell = function(row, col, color) {
-    canM.fillStyle = '#000000';
-    canM.fillRect(cellWidth*col,cellHeight*row, cellWidth, cellHeight);
     canM.fillStyle = color;
-    canM.fillRect((cellWidth)*col + 1 ,(cellHeight)*row + 1, (cellWidth-1), (cellHeight-1));
+    var x = col * (gapSize + cellWidth) + gapSize;
+    var y = row * (gapSize + cellHeight) + gapSize;
+    canM.fillRect(x, y, cellWidth, cellHeight);
   };
 
   that.getCanvas = function() {
@@ -48,13 +32,29 @@ var ChartView = function(cellWidth, cellHeight, model, canvas) {
 
   that.getCell = function(mouseEvt) {
     var rect = canvas.getBoundingClientRect();
-    mouseX = Math.floor(mouseEvt.clientX - rect.left);
-    mouseY = Math.floor(mouseEvt.clientY - rect.top);
-    return { 
-      col: Math.floor(mouseX / cellWidth), 
-      row: Math.floor(mouseY / cellHeight)
-    };
+    var mouseX = Math.floor(evt.clientX - rect.left);
+    var mouseY = Math.floor(evt.clientY - rect.top);
+    if ((mouseX % (gap + cellWidth)) < gapSize) {
+      return null;
+    } else if ((mouseY % (gapSize + cellHeight)) < gapSize) {
+      return null;
+    } else {
+      return { 
+        col: Math.floor(mouseX / (gapSize + cellWidth)), 
+        row: Math.floor(mouseY / (gapSize + cellHeight))
+      };
+    }
   };
+
+  // clear and initialize grid color
+  canM.clearRect(0, 0, canvas.width, canvas.height);
+  canM.fillStyle = '#000000';
+  canM.fillRect(0, 0, canvas.width, canvas.height);
+  for (var i=0; i < colSize; i++) {
+    for (var j=0; j < rowSize; j++) {
+      that.colorCell(j, i, '#FFFFFF');
+    }
+  }
 
   Object.freeze(that);
   return that;
