182dd5f7551524153e7c50bd6dd78e69437c767c
Denis Li <denisli@mit.edu>
2016-11-23 01:25:17 -0500

    documented chart_controller.js

--- stitch_hub/public/javascript/chart_controller.js
+++ stitch_hub/public/javascript/chart_controller.js
@@ -1,3 +1,10 @@
+/**
+* Sets the color of a cell in a chart to a specific color
+*
+* @param view the view representing how the chart looks
+* @param model the model representing the data of the chart
+* @param color the color to use
+*/
 var clickCell = function (view, model, color){
 	var cell = view.getCell(event);
   if (cell != null) {
