8032cf3df56f2516ab7e0063942d31a0dd22dd48
Denis Li <denisli@mit.edu>
2016-11-23 01:29:59 -0500

    documented ratio.js

--- stitch_hub/public/javascript/ratios.js
+++ stitch_hub/public/javascript/ratios.js
@@ -1,17 +1,19 @@
+/**
+* Given a type, returns an array representing the desired aspect-ratio
+* of the cells in such a type of chart.
+*/
 var getRatio = function(type){
-      var ratios = {
-        'CROSS_STITCH': [4,4],
-        'KNIT_V': [4,5],
-        'KNIT_H': [5,4],
-        'CROCHET_V': [3,4],
-        'CROCHET_H': [4,3]
+  var ratios = {
+    'CROSS_STITCH': [4,4],
+    'KNIT_V': [4,5],
+    'KNIT_H': [5,4],
+    'CROCHET_V': [3,4],
+    'CROCHET_H': [4,3]
+  };
 
-          };
+  var size = ratios[type];
+  var xscale = size[0];
+  var yscale = size[1];
 
-      var size = ratios[type];
-      var xscale = size[0];
-      var yscale = size[1];
-
-      return [type,xscale,yscale];
-
-    };
+  return [type,xscale,yscale];
+};
