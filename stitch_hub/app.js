d77f17825b2d5efd8e22aff195b536e3ef4b99c0
Denis Li <denisli@mit.edu>
2016-11-23 01:04:27 -0500

    cleaned up app.js and added route for handling 404 page

--- stitch_hub/app.js
+++ stitch_hub/app.js
@@ -5,13 +5,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 
-
-// var routes = require('./routes/index');
-
-
+// connect with mongoose
 var mongoose = require('mongoose');
-
-//**to test database need to specify location of database below!
 mongoose.connect('mongodb://localhost/my_database8');
 
 var db = mongoose.connection;
@@ -22,31 +17,12 @@ db.once('open', function (callback) {
 
 var app = express();
 
-
-////// NEED TO UPDATE THIS 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
-
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
-// app.use('/', routes);
-
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-
-// homepage
+// homepage route
 app.get('/', function(req, res) {
     fs.readFile('index.html', 'utf-8', function(err, page) {
       res.write(page);
@@ -54,32 +30,14 @@ app.get('/', function(req, res) {
     });
 });
 
+// routes for charts
 var charts = require('./routes/charts_routes.js');
 app.use('/charts', charts);
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-// if (app.get('env') === 'development') {
-//   app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//       message: err.message,
-//       error: err
-//     });
-//   });
-// }
-
-// production error handler
-// no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.render('error', {
-//     message: err.message,
-//     error: {}
-//   });
-// });
+// 404 Route
+app.use(function(req, res){
+  res.status(404).end('404 Error: Page not found');
+})
 
 var port = 3000;
 app.listen(port, function() {
