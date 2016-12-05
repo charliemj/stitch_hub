aee973d3935a9d4b1ad667463e705f8dc1b49098
James Usrey <jusrey@mit.edu>
2016-12-04 23:43:19 -0500

    made ajax calls to login/signup.
    maybe it works?

--- stitch_hub/public/javascript/login_signup.js
+++ stitch_hub/public/javascript/login_signup.js
@@ -22,10 +22,64 @@ window.onclick = function(event) {
 }
 
 
+$('#LogButton').on('click', function() {
+    var username = document.getElementById("SU_uname").value;
+    var password = document.getElementById("SU_psw").value;
 
 
+    $.ajax({
+      url: '/login',
+      method: 'POST',
+      data: {
+        username: username,
+        password: password,
 
 
+      },
+      success: function() {
+        console.log("successfully logged in");
+      },
+      error: function(error) {
+        console.log('Error logging in');
+        console.log(error);
+      }
+
+    });//end ajax
+
+
+  }); //end login-button handler
+
+
+$('#RegButton').on('click', function() {
+    var username = document.getElementById("SU_uname").value;
+    var password = document.getElementById("SU_psw").value;
+    var email = document.getElementById("email").value;
+    var bday = document.getElementById("bday").value;
+
+
+    $.ajax({
+      url: '/signup',
+      method: 'POST',
+      data: {
+        username: username,
+        password: password,
+        email: email,
+        bday: bday
+
+
+      },
+      success: function() {
+        console.log("successfully registered");
+      },
+      error: function(error) {
+        console.log('Error registering');
+        console.log(error);
+      }
+
+    });//end ajax
+
+
+  }); //end signup-button handler
 
 
 
