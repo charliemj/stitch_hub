a0676e3b9279d0ff7f6206880826a2509a07be96
James Usrey <jusrey@mit.edu>
2016-12-04 21:20:48 -0500

    Made login/signup page.
    Doesn't actually do anything yet.

--- stitch_hub/public/javascript/login_signup.js
+++ stitch_hub/public/javascript/login_signup.js
@@ -5,33 +5,30 @@ $(document).ready(function() {
   
   
 
-  // add event listener so that login-button will login when clicked
-  $('#login-button').on('click', function() {
-    var username = document.getElementById("loguname");
-    var password = document.getElementById("logpsw");
+// Get the login modal
+var loginmodal = document.getElementById('loginbox');
+
+// Get the signup modal
+var signupmodal = document.getElementById('signupbox');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == loginmodal) {
+        loginmodal.style.display = "none";
+
+    } else if (event.target == signupmodal) {
+        signupmodal.style.display = "none";
+    }
+}
 
 
-    $.ajax({
-      url: '/login',
-      method: 'POST',
-      data: {
-        username: document.getElementById("title").value,
-        password: document.getElementById("description").value,
-
-      },
-      success: function() {
-        console.log("successfully logged in");
-        window.location.replace("/");
-      },
-      error: function(error) {
-        console.log('Error logging in');
-        console.log(error);
-      }
-
-    });//end ajax
 
 
-  }); //end login-button handler
+
+
+
+
+
 
 
 });
