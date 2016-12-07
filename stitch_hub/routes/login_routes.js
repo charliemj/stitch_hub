1a000385a9ee985cbf9f102e4634d8a24390dd6f
Karleigh Moore <karleighmoore19@gmail.com>
2016-12-06 19:42:41 -0500

    deleted repeated code that was breaking everything

--- stitch_hub/routes/login_routes.js
+++ stitch_hub/routes/login_routes.js
@@ -11,12 +11,12 @@ router.post('/', function (req, res) {
   Users.findOne({ username: req.body.username, password: req.body.password }, function (err, user) {
     if (user) {
       req.session.username = req.body.username;
-      res.send({loggedIn: true, username: req.body.username, userId: user._id});
+      // res.send({loggedIn: true, username: req.body.username, userId: user._id});
 
       req.session.userId = user._id; 
-      
-      res.send({loggedIn: true,userId: user._id });
       console.log("req.session.userId",req.session.userId, "user._id", user._id);
+      res.send({loggedIn: true,userId: user._id });
+      
 
     } else {
       res.send({loggedIn: false});
