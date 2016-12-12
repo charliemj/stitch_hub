0ca34ecfec0202b29639043a0fababa208888d00
James Usrey <jusrey@mit.edu>
2016-12-12 01:51:14 -0500

    fix for heroku

--- stitch_hub/app.js
+++ stitch_hub/app.js
@@ -4,15 +4,17 @@ var fs = require('fs');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var passport = require('passport');
 
-
+// TODO: use passport instead of these
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 
 // connect with mongoose
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/my_database8');
-
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/my_database25');
+//mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/mymongodb');
+//mongoose.connect('mongodb://kjmoore:password@ds063946.mlab.com:63946/heroku_vqt01pgt');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -27,16 +29,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public')); //TODO what does this do?
 
+// TODO: use passport instead of cookieParser
 app.use(cookieParser());
 app.use(session({
   secret: 'supersecret',
   resave: true,
   saveUninitialized: true,
-  cookie: {secure: false}
+  cookie: {secure: false},
 }));
 
 //app.use(express.session({ secret: 'keyboard cat' })); //TODO replace above line with this?
-
+app.use(passport.initialize());
+app.use(passport.session());
 
 // homepage route
 app.get('/', function(req, res) {
@@ -54,10 +58,6 @@ app.use('/charts', charts);
 var users = require('./routes/users_routes.js');
 app.use('/users', users);
 
-// route for current user
-var currentUserRouter = require('./routes/current_user_routes.js');
-app.use('/currentUser', currentUserRouter);
-
 // routes for login/logout
 var login = require('./routes/login_routes.js');
 app.use('/login', login);
@@ -69,15 +69,15 @@ var like = require('./routes/likes_routes.js');
 app.use('/like', like);
 
 // routes for comments
-var comments = require('./routes/comment_routes.js');
-app.use('/comments', comments);
+var comment = require('./routes/comment_routes.js');
+app.use('/comment', comment);
 
 // 404 Route
 app.use(function(req, res){
   res.status(404).end('404 Error: Page not found');
 });
 
-var port = 3000;
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Server listening on http://localhost:" + port);
 });
