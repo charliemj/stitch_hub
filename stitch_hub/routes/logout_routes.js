ca950243b6dbe0e994b5da429692e7e632cd614b
Val Healy <valiant@mit.edu>
2016-12-11 21:58:09 -0500

    documented routes

--- stitch_hub/routes/logout_routes.js
+++ stitch_hub/routes/logout_routes.js
@@ -4,10 +4,15 @@ var mongoose = require('mongoose');
 var Users = require('../model/user_model.js');
 var session = require('express-session');
 
+/**
+ * Handles POST request for logout. //TODO should have error handling!
+ *
+ * Responds with message--> loggedOut:true, success:"logged out!"
+ */
 router.post('/', function (req, res) {
   req.session.username = null;
   req.session.userId = null;
-  res.send({loggedOut: true, sucess:"logged out!"});
+  res.send({loggedOut: true, success:"logged out!"});
 });
 
 module.exports = router;
